0869c2bf4cdc7ce146b7e4ebf78ad20225f6a4c2
Bruna Pires <bruna.gpires@hotmail.com>
2022-08-10 15:59:50 -0300

    Exercicio fixacao Dictionary

---
+++ ExDictionary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int vote = int.Parse(line[1]);
+                        if (dictionary.ContainsKey(name))
+                        {
+                            dictionary[name] += vote;
+                        }
+                        else
+                        {
+                            dictionary[name] = vote;
+                        }
+                    }
+                    foreach(var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
